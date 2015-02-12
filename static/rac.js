7d9755abe56fbab4f8ecd81c42cd8318be51ad88
aheadley <aheadley@nexcess.net>
2015-02-12 16:24:57 -0500

    Refactor JS from agent collection page into separate script file

--- static/rac.js
+++ static/rac.js
@@ -22,34 +22,71 @@
  * @link http://kilianvalkhof.com/2010/javascript/how-to-build-a-fast-simple-list-filter-with-jquery/
  */
 
-// custom css expression for a case-insensitive contains()
+ function agent_collection_updateAgentList(idx, host) {
+    $("#host-group-item-"+host.id).attr({class: "list-group-item list-group-item-default"});
+    $("#host-spinner-"+host.id).show();
+    $.getJSON(host.url_list_clients_json,
+        function(response_data) {
+            $.each(response_data, function(policy_id, data) {
+                if(!data.agent) {
+                    $("#host-group-item-"+host.id).addClass("list-group-item-warning");
+                    return;
+                }
+                var row = $("<tr>").attr({id: policy_id}),
+                    type = $("<td>"),
+                    hostname = $("<td>").text(data.agent.hostname),
+                    description = $("<td>").text(data.agent.description),
+                    cdp_host = $("<td>").html("<a href=\""+host.external_link+"\">"+host.hostname+"</a>"),
+                    rp_limit = $("<td>").text(data.policy.recoveryPointLimit),
+                    db_module = $("<td>"),
+                    state = $("<td>").text(data.policy.state);
+                if(data.agent.databaseAddOnEnabled &&
+                        data.policy.databaseInstanceList &&
+                        data.policy.databaseInstanceList.length > 0) {
+                    db_module.html("<span class=\"label label-success\">Yes</span>");
+                } else {
+                    db_module.html("<span class=\"label label-danger\">No</span>");
+                }
+                switch(data.agent.osType) {
+                    case "LINUX":
+                        $(type).append($("<i>").attr({title: data.agent.osType,
+                            class: "fa fa -lg fa-linux"}));
+                        break;
+                    default:
+                        $(type).append($("<i>").attr({title: data.agent.osType,
+                            class: "fa fa -lg fa-question-circle"}));
+                }
+                switch(data.policy.state) {
+                    case "ERROR":
+                        $(row).attr({class: "danger"});
+                        break;
+                    case "ALERT":
+                        $(row).attr({class: "warning"});
+                        break;
+                    case "OK":
+                        $(row).attr({class: "success"});
+                        break;
+                }
+                if(!data.policy.enabled) {
+                    $(row.attr({class: "default"}));
+                }
 
-// jQuery.expr[':'].Contains = function(a,i,m){
-//     return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
-// };
+                $.each([type, hostname, description, cdp_host, rp_limit, db_module, state],
+                    function(i, v) {
+                        $(row).append(v);
+                    });
+                $(row).appendTo($("#agents-list-table"));
+            });
 
-function addCollectionFilter(attach_point, collection, filter_item) {
-    // header is any element, list is an unordered list
-
-    // create and add the filter form to the header
-    var form = $("<form>").attr({"class":"filterform pull-right", "action":"#"}),
-        input = $("<input>").attr({"class":"filterinput", "type":"text",
-            "placeholder": "Search..."});
-    $(form).append(input).appendTo(attach_point);
-
-    $(input).change(function () {
-        var filter = $(this).val();
-        if(filter) {
-            // this finds all links in a list that contain the input,
-            // and hide the ones not containing the input while showing the ones that do
-            $(collection).find(filter_item + ":not(:Contains(" + filter + "))").parent().hide();
-            $(collection).find(filter_item + ":Contains(" + filter + ")").parent().show();
-        } else {
-            $(collection).find(filter_item).show();
+            $("#host-group-item-"+host.id).addClass("list-group-item-success");
+            $("#agents-list-table").trigger("update");
         }
-        return false;
-    }).keypress(function () {
-        // fire the above change event after every letter
-        $(this).change();
+    ).fail(function() {
+        $("#host-group-item-"+host.id).addClass("list-group-item-danger")
+            .click(function() {
+                agent_collection_updateAgentList(idx, host)
+            });
+    }).always(function() {
+        $("#host-spinner-"+host.id).hide();
     });
-};
+ }
